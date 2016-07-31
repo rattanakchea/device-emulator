126d81f282205fed828e64aa46d021a4e9e8e6e1
Rattanak Chea <rattanak.chea@gmail.com>
2016-07-31 13:34:27 -0700

    import images

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
-(function () {
-    
-  var currentDevice = $( "select option:selected" ).val();
-  var appFrame = $('div#app-frame');
-  var appWrapper = $('div#app-wrapper');
-  console.log('currentDevice: ', currentDevice);
+(function() {
 
-  var skinImages = {
-  	androidPhone: 'https://creator.ionic.io/img/android-skin.png',
-  	iphone: 'https://creator.ionic.io/img/iphone-skin.png',
-  	ipad: 'https://creator.ionic.io/img/ipad-skin.png'
-  };
+    var currentDevice = $("select option:selected").val();
+    var appFrame = $('div#app-frame');
+    var appWrapper = $('div#app-wrapper');
+ 
+    var skinImages = {
+        androidPhone: '../img/android-skin.png',
+        iphone: '../img/iphone-skin.png',
+        ipad: '../img/ipad-skin.png'
+    };
 
-  var devices = {
-  	androidPhone: 'android-phone',
-  	iphone: 'iphone',
-  	ipad: 'ipad'
-  };
+    var devices = {
+        androidPhone: 'android-phone',
+        iphone: 'iphone',
+        ipad: 'ipad'
+    };
 
-  var skin = $('#app-wrapper');
+    var skin = $('#app-wrapper');
 
-    $('select').on('change', function(){
-    	var device = this.value;
+    $('select').on('change', function() {
+        var device = this.value;
 
-    	if (device == devices.iphone){    	
-    		console.log('iphone skin');
-    		skin.css('background-image', 'url(' + skinImages.iphone + ')' );
-    		appFrame.css('height', '530px');
+        if (device == devices.iphone) {
+            skin.css('background-image', 'url(' + skinImages.iphone + ')');
+            appFrame.css('height', '530px');
 
-    	} else if (device == devices.androidPhone){
-    		skin.css('background-image', 'url(' + skinImages.androidPhone + ')' );
-    		appFrame.css('height', '505px');
+        } else if (device == devices.androidPhone) {
+            skin.css('background-image', 'url(' + skinImages.androidPhone + ')');
+            appFrame.css('height', '505px');
 
-    	} else if (device == devices.ipad){
-    		skin.css('background-image', 'url(' + skinImages.ipad + ')' );
-    		appFrame.css('height', '570px');
-    	}
+        } else if (device == devices.ipad) {
+            skin.css('background-image', 'url(' + skinImages.ipad + ')');
+            appFrame.css('height', '570px');
+        }
     });
 
 })();
