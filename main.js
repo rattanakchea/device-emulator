164423cff97fd5121f14fac8315d8c972f8fd731
Rattanak Chea <rattanak.chea@gmail.com>
2016-07-31 13:24:28 -0700

    calc screen width by %, add styling

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 (function () {
     
   var currentDevice = $( "select option:selected" ).val();
+  var appFrame = $('div#app-frame');
+  var appWrapper = $('div#app-wrapper');
   console.log('currentDevice: ', currentDevice);
 
   var skinImages = {
@@ -15,23 +17,23 @@
   	ipad: 'ipad'
   };
 
-  var skin = $('#app-test');
+  var skin = $('#app-wrapper');
 
     $('select').on('change', function(){
-
     	var device = this.value;
-
-    	// if (device == currentDevice){
-    	// 	return;
-    	// }
 
     	if (device == devices.iphone){    	
     		console.log('iphone skin');
     		skin.css('background-image', 'url(' + skinImages.iphone + ')' );
+    		appFrame.css('height', '530px');
+
     	} else if (device == devices.androidPhone){
     		skin.css('background-image', 'url(' + skinImages.androidPhone + ')' );
+    		appFrame.css('height', '505px');
+
     	} else if (device == devices.ipad){
     		skin.css('background-image', 'url(' + skinImages.ipad + ')' );
+    		appFrame.css('height', '570px');
     	}
     });
 
